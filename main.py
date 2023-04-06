7a9c9e78b0441715f966493557488127d129cc0a
jensentke <jensentke@gmail.com>
2023-04-05 21:16:19 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+import numpy as np
+
+a = np.arange(15).reshape(3, 5)
+print(a.shape)
+print(a.dtype.name)
+
+
+
